2b5faa9d5caa41122c276ec27f73c2c481099593
MisaSohee <sohee5951@naver.com>
2024-01-20 22:48:03 +0900

    환경세팅 완료

---
+++ src/test/java/com/cos/blog/BlogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cos.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
